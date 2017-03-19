7f4bc22eaa74bec3bb966c0c540c1d5b3ed27351
BlazPocrnja <bpocrnja@gmail.com>
2017-03-19 14:56:39 -0600

    xreverse defined

--- 1472712.pl
+++ 1472712.pl
@@ -3,3 +3,13 @@ CMPUT 325 Assignment 3
 Author: Blaz Pocrnja
 Student ID: 1472712
 */
+
+/*	Question 1
+*/
+xreverse(L, R) :- xreverse(L, R, []).
+xreverse([], R, R).
+xreverse([Front|Rest], R, Accum) :- xreverse(Rest, R, [Front|Accum]).
+
+/*	Question 2
+*/
+
