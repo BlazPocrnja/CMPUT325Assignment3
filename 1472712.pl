b7c10590dfc1d1f84c8620de75d85cb45214a833
BlazPocrnja <bpocrnja@gmail.com>
2017-03-19 22:16:28 -0600

    Finished removeLast, added clique predicates

--- 1472712.pl
+++ 1472712.pl
@@ -12,12 +12,31 @@ xreverse([Front|Rest], R, Accum) :- xreverse(Rest, R, [Front|Accum]).
 
 /*	Question 2
 */
-xunique(L,O) :- xunique(L, O, []).
+xunique(L, O) :- xunique(L, O, []).
 xunique([], O, O).
 xunique([F|R], O, Accum) :- member(F, Accum), xunique(R, O, Accum). 
 xunique([F|R], O, Accum) :- \+ member(F, Accum), append(Accum, [F], L), xunique(R, O, L).
 
-/* Question 3
+/*	Question 3
 */
-xdiff(L1, L2, L):- subtract(L1, L2, L3), xunique(L3, L).
+xdiff(L1, L2, L) :- subtract(L1, L2, L3), xunique(L3, L).
 
+/*	Question 4
+*/
+removeLast(L, L1, Last) :- xreverse(L, [F|R]), xreverse(R,L1), Last = F.
+
+/*	Question 5
+*/
+clique(L) :- findall(X,node(X),Nodes), xsubset(L,Nodes), allConnected(L).
+
+xsubset([], _).
+xsubset([X|Xs], Set) :- xappend(_, [X|Set1], Set), xsubset(Xs, Set1).
+
+xappend([], L, L).
+xappend([H|T], L, [H|R]) :- xappend(T, L, R).
+
+/*	Question 5.1
+*/
+
+/*	Question 5.2
+*/
